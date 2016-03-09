0a8f3b82e3ca0587cc2d1bfaeab02ae37b1efac8
smital <vojtech.smital@embedit.cz>
2016-03-09 09:35:52 +0100

    connect message structure updated according to new protocol

--- src/main/java/soc/qase/tools/com/CommunicationMessageBuilder.java
+++ src/main/java/soc/qase/tools/com/CommunicationMessageBuilder.java
@@ -12,7 +12,7 @@ public class CommunicationMessageBuilder {
     private static final String KEY_VALUE_ENTRY_DELIMITER = "\\";
 
     private static final String CONNECT_MESSAGE_PREFIX = "connect ";
-    private static final String DAIKATANA_VERSION_MESSAGE_SNIPPET = "version" + KEY_VALUE_DELIMITER + "Daikatana v1.3 for x86. Built Dec 30 2014 at 02:52:40";
+    private static final String DAIKATANA_VERSION_MESSAGE_SNIPPET = "version" + KEY_VALUE_DELIMITER + "Daikatana v1.3 for x86. Built Mar  6 2016 at 21:14:31";
     private static final String CHAT_MESSAGE_SNIPPET = "chat" + KEY_VALUE_DELIMITER + "3";
     private static final String DM_TAUNTS_MESSAGE_SNIPPET = "dm_taunts" + KEY_VALUE_DELIMITER + "1";
     private static final String CTF_TEAM_MESSAGE_SNIPPET = "ctf_team" + KEY_VALUE_DELIMITER + "0";
@@ -20,7 +20,7 @@ public class CommunicationMessageBuilder {
     private static final String CL_CINEMATICS_MESSAGE_SNIPPET = "cl_cinematics" + KEY_VALUE_DELIMITER + "1";
     private static final String FOV_MESSAGE_SNIPPET = "fov" + KEY_VALUE_DELIMITER + "$FOV_VALUE";
     private static final String MSG_MESSAGE_SNIPPET = "msg" + KEY_VALUE_DELIMITER + "$MSG_VALUE";
-    private static final String CL_AUTOWEAPONSWITCH_MESSAGE_SNIPPET = "cl_autowweaponswitch" + KEY_VALUE_DELIMITER + "1";
+    private static final String CL_AUTOWEAPONSWITCH_MESSAGE_SNIPPET = "cl_autowweaponswitch" + KEY_VALUE_DELIMITER + "0";
     private static final String DT_TEAM_MESSAGE_SNIPPET = "dt_team" + KEY_VALUE_DELIMITER + "0";
     private static final String RATE_MESSAGE_SNIPPET = "rate" + KEY_VALUE_DELIMITER + "$RATE_VALUE";
     private static final String TEAM_MESSAGE_SNIPPET = "team" + KEY_VALUE_DELIMITER + "4";
@@ -29,6 +29,8 @@ public class CommunicationMessageBuilder {
     private static final String SKINNAME_MESSAGE_SNIPPET = "skinname" + KEY_VALUE_DELIMITER + "$SKINNAME_VALUE";
     private static final String SKINCOLOR_MESSAGE_SNIPPET = "skincolor" + KEY_VALUE_DELIMITER + "0";
     private static final String CHARACTER_MESSAGE_SNIPPET = "character" + KEY_VALUE_DELIMITER + "0";
+    private static final String SPECTATOR_MESSAGE_SNIPPET = "spectator" + KEY_VALUE_DELIMITER + "0";
+    private static final String CL_POWERUP_SOUNDS_SNIPPET = "cl_powerup_sounds" + KEY_VALUE_DELIMITER + "3";
 
 
     private CommunicationMessageBuilder() {
@@ -40,14 +42,16 @@ public class CommunicationMessageBuilder {
 
         String result = CONNECT_MESSAGE_PREFIX + ServerTemporaryEntity.getServerVersion() + " " + clientID + " " + challengeNumber;
         result = result + " \"" + KEY_VALUE_ENTRY_DELIMITER + DAIKATANA_VERSION_MESSAGE_SNIPPET;
+        result = result + KEY_VALUE_ENTRY_DELIMITER + CL_AUTOWEAPONSWITCH_MESSAGE_SNIPPET;
+        result = result + KEY_VALUE_ENTRY_DELIMITER + SPECTATOR_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + CHAT_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + DM_TAUNTS_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + CTF_TEAM_MESSAGE_SNIPPET;
+        result = result + KEY_VALUE_ENTRY_DELIMITER + CL_POWERUP_SOUNDS_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + CHATMSG_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + CL_CINEMATICS_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + FOV_MESSAGE_SNIPPET.replaceAll("\\$FOV_VALUE", String.valueOf(user.getFov()));
         result = result + KEY_VALUE_ENTRY_DELIMITER + MSG_MESSAGE_SNIPPET.replaceAll("\\$MSG_VALUE", String.valueOf(user.getMessage()));
-        result = result + KEY_VALUE_ENTRY_DELIMITER + CL_AUTOWEAPONSWITCH_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + DT_TEAM_MESSAGE_SNIPPET;
         result = result + KEY_VALUE_ENTRY_DELIMITER + RATE_MESSAGE_SNIPPET.replaceAll("\\$RATE_VALUE", String.valueOf(user.getRate()));
         result = result + KEY_VALUE_ENTRY_DELIMITER + TEAM_MESSAGE_SNIPPET;
